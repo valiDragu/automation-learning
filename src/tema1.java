18b423ca2bacd2a64544ded61fd4772232709323
valiDragu <valismd2393+git@gmail.com>
2026-02-21 10:53:10 +0200

    Am adaugat rezolvarile pentru exercitiile din tema. Referinte: tema1 si tema1_continuare

---
+++ src/tema1.java
@@ -0,0 +1,85 @@
+public class tema1 {
+    public static void main() {
+        /*
+        Creează variabile pentru: varsta unui student, inaltimea acestuia, daca este prezent la curs.
+        Afiseaza datele prin mesaje/propozitii.
+         */
+        int varstaStudent = 20;
+        double inaltimeStudent = 1.81;
+        boolean prezentLaCurs = false;
+
+        System.out.println("Varsta studentului: " + varstaStudent);
+        System.out.println("Inaltimea studentului: " + inaltimeStudent);
+//        System.out.println("Este studentul prezent la curs? " + prezentLaCurs);
+        if (prezentLaCurs) {
+            System.out.println("Studentul este prezent la curs");
+        } else {
+            System.out.println("Studentul nu este prezent la curs");
+        }
+
+        System.out.println();
+
+
+        /*
+        Crează doua variabile int si una double. Calculeaza suma celor doua valori int. Calculeaza media dintre toate 3.
+        */
+        int primulNumar = 23;
+        int alDoileaNumar = 45;
+        double alTreileaNumar = 2.09;
+
+//        System.out.println(primulNumar + alDoileaNumar);
+//        System.out.println((primulNumar + alDoileaNumar + alTreileaNumar)  / 3);
+
+        int produsulADouaNumere = primulNumar + alDoileaNumar;
+        double mediaNumerelor = (produsulADouaNumere + alTreileaNumar) / 3;
+
+        System.out.println("Produsul primelor 2 numere: " + produsulADouaNumere);
+        System.out.println("Media celor 3 numere: " + mediaNumerelor);
+        System.out.println();
+
+
+        /*
+        Creaza o variabila pentru varsta. Dacă persoana are 18 sau mai mult, afișează: “Acces permis”, altfel “Acces interzis”.
+         */
+        int varsta = 17;
+
+        System.out.println("Afiseaza nivelul de acces");
+
+        if (varsta >= 18) {
+            System.out.println("Acces permis");
+        } else {
+            System.out.println("Acces interzis");
+        }
+
+        System.out.println();
+
+
+        /*
+        Calculeaza suma numerelor de la 20 la 50
+         */
+        int sumaNumerelor = 0;
+        for (int i = 20; i <= 50; i ++) {
+            sumaNumerelor += i;
+        }
+
+        System.out.println("Calculeaza suma numerelor de la 20 la 50");
+        System.out.println("Suma numerelor de la 20 la 50 este: " + sumaNumerelor);
+        System.out.println();
+
+
+        /*
+        Afișează numerele de la 10 la 1 folosind while.
+         */
+        int start = 1;
+        int stop = 10;
+
+        System.out.println("Afișează numerele de la 10 la 1 folosind while:");
+
+        while (start <= stop) {
+            System.out.print(start + " ");
+            start++;
+        }
+
+        System.out.println();
+    }
+}
---
+++ src/tema1_continuare.java
@@ -0,0 +1,69 @@
+public class tema1_continuare {
+
+    // Creează o metodă care afișează: “Invat Java!”
+    public static void mesaj() {
+        System.out.println("Invat Java!");
+    }
+
+
+    // Creaza o metoda cu 2 parametrii int, care returneaza produsul celor doi parametrii.
+    public static int produs(int x, int y) {
+        return x + y;
+    }
+
+
+    // Calculează suma numerelor pare de la 1 la 50. Crează o metoda care returneaza rezultatul.
+    public static int sumaSir(int start, int stop) {
+        int sum = 0;
+        while (start <= stop) {
+            sum += start;
+            start++;
+        }
+
+        return sum;
+    }
+
+
+    // Creaza o metoda cu 3 parametrii. Returneaza media celor trei parametrii.
+    public static int mediaATreiNumere(int a, int b, int c) {
+        return (a + b + c) / 3;
+    }
+
+
+    //  Creeaza o metoda care primeste 2 parametrii: start si end.
+    //  Afiseaza toate numerele cuprinse intre aceste doua numere.
+    public static void afisareaUnuiSir(int start, int end) {
+        if (start <= end) {
+            while (start <= end) {
+                System.out.print(start + " ");
+                start++;
+            }
+        } else {
+            System.out.println("Primul numar trebuie sa fie mai mic decat al doilea numar");
+        }
+
+    }
+
+
+
+    public static void main() {
+        mesaj();
+        System.out.println();
+
+        int suma = produs(10, 23);
+        System.out.println("Produsul a doua numere: " + suma);
+        System.out.println();
+
+        int sumaSirului = sumaSir(1, 50);
+        System.out.println("Suma numerelor de la 1 la 50: " + sumaSirului);
+        System.out.println();
+
+        int mediaNumerelor = mediaATreiNumere(1, 2,3);
+        System.out.println("Media a trei numere: " + mediaNumerelor);
+        System.out.println();
+
+        System.out.println("Afisarea numerelor dintr-un sir");
+        afisareaUnuiSir(10, 23);
+        System.out.println();
+    }
+}
