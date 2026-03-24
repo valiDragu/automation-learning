3fb1029f5dedc097bdae4222de8e5fefe3cae8eb
valiDragu <valismd2393+git@gmail.com>
2026-03-24 17:54:04 +0200

    - Tema 5: added AfterMethod

--- src/test/java/tema5/tema5.java
+++ src/test/java/tema5/tema5.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -25,10 +26,10 @@ public class tema5 {
     }
 
 
-//    @AfterMethod
-//    public void teardown() {
-//        driver.quit();
-//    }
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
 
 
     /*
